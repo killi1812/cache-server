cfd884f11fc1fde0c87d6009f1efd04977fcd655
killi1812 <cvok.fran@gmail.com>
2026-02-11 11:13:16 +0100

    Service model

---
+++ src/model/agent.go
@@ -0,0 +1,11 @@
+package model
+
+import "gorm.io/gorm"
+
+type Agent struct {
+	gorm.Model
+	Name  string `gorm:"type:varchar(100);unique;not null"`
+	token string `gorm:"type:varchar(255);not null"`
+
+	// optinal agent
+}
